10f73b5a592600c23c14e3e047555f04e2f67cda
Jonathan Urias <jonathan@ycharts.com>
2025-06-16 07:16:32 -0700

    Enable Render domain in ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-o2&+rk(f7pg=9e36+kso41i%ex&u$)4%f8xx)@*8305(giu%lh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
 
 # Application definition
